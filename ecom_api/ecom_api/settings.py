26b2119a164ccda6931ea5ba5fd8bc254325db66
Prashant Sachan <your-email@example.com>
2026-02-02 01:24:28 +0530

    feat: Add localhost:5174 to CORS_ALLOWED_ORIGINS for frontend access

--- ecom_api/ecom_api/settings.py
+++ ecom_api/ecom_api/settings.py
@@ -240,6 +240,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:5174",
 ]
 
 if os.getenv("FRONTEND_URL"):
